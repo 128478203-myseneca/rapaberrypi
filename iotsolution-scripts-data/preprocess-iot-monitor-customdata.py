7e54a87f7bdd96f0897858b3518e79e59b20f821
Istvan Feher <62614194+128478203-myseneca@users.noreply.github.com>
2023-07-24 18:03:56 -0400

    process variable

--- iotsolution-scripts-data/preprocess-iot-monitor-customdata.py
+++ iotsolution-scripts-data/preprocess-iot-monitor-customdata.py
@@ -196,11 +196,11 @@ latlong=lat:long'
      usemysql=1
 
 #     streamstojoin="Current,Voltage,Power"
-     streamstojoin="Current,Voltage,Power,EnergyUsed,EnergyUsed24hr"
+     streamstojoin="Current,Voltage,Power"
  
      identifier = "IoT device performance and failures"
 
-     preprocesslogic='avg,min,max,avg,outliers'
+     preprocesslogic='min,max,avg'
 
      
 #     pathtotmlattrs='oem=id,lat=subject.reference,long=component.0.code.coding.0.display,location=component.1.valueQuantity.value'     
